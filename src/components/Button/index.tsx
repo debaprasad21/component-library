90b0e135d848f528f89fba69c5c495736d200a63
Debaprasad Sahoo <debaprasadworld@gmail.com>
2024-04-20 16:00:22 +0530

    adding storybook and running button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,11 +3,7 @@ import { ComponentProps } from "react";
 type ButtonProps = ComponentProps<"button">;
 
 const Button = ({ ...props }: ButtonProps) => {
-  return (
-    <button className="text-blue-500" {...props}>
-      Hello
-    </button>
-  );
+  return <button className="text-blue-500" {...props} />;
 };
 
 export default Button;
